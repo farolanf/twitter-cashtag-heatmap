f69c1cc836e4f6dee97953fa82857453ee602c3c
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2017-05-01 23:35:18 -0300

    added mysql functionality

--- README.md
+++ README.md
@@ -7,11 +7,22 @@ Running Locally
 =======================
 
 Running this application locally requires that you input four twitter API keys. I cannot provide you with these
-keys since only one streaming connection can be made for each. However, you can go to https://apps.twitter.com/, 
+keys since only one streaming connection can be made for each. However, you can go to https://apps.twitter.com/,
 create a new application, and use the API keys from that in this application. Don't worry, it's free ;)
 
 Once you have the API keys you'll just need to insert them into the app.js file. The placeholders should be
 easy to find as they're at the top of the file with a big IMPORTANT stamp on them.
+
+To create the MySQL database and tables:
+
+```
+CREATE DATABASE twitterCashtagHeatmap;
+USE twitterCashtagHeatmap;
+CREATE TABLE watchList (symbol_id INT AUTO_INCREMENT KEY, cash_tag VARCHAR(7), active BOOL);
+CREATE TABLE history (history_id INT AUTO_INCREMENT KEY, when_it_happened DATETIME, symbol_id INT, number_of_twitts INT);
+CREATE TABLE history_diff (when_it_happened DATETIME, symbol INT, diff INT);
+\q
+```
 
 Screenshot
 =======================
--- app.js
+++ app.js
@@ -2,12 +2,13 @@
  * Module dependencies.
  */
 var express = require('express')
-  , io = require('socket.io')
-  , http = require('http')
-  , twitter = require('ntwitter')
-  , cronJob = require('cron').CronJob
-  , _ = require('underscore')
-  , path = require('path');
+    , io = require('socket.io')
+    , http = require('http')
+    , twitter = require('ntwitter')
+    , cronJob = require('cron').CronJob
+    , _ = require('underscore')
+    , mysql = require('mysql')
+    , path = require('path');
 
 //Create an express app
 var app = express();
@@ -25,7 +26,7 @@ var server = http.createServer(app);
 // var access_token = '';          // <---- Fill me in
 // var access_token_secret = '';   // <---- Fill me in
 
-var api_key = 'C4APRTiFDPbu4f83bxu2crM3X'; 
+var api_key = 'C4APRTiFDPbu4f83bxu2crM3X';
 var api_secret = 'GVXhTV7B7hSeI9i5p7Eg6raKsCTattD7qpTKClPKPVtqpfuxr4';
 var access_token = '503627811-RGLyEFBvtbP7IUp1FRBU2OBHphqopUqymsJCgfGK';
 var access_token_secret = 'CzFaJMS8hwZMXtip5G2i4M7dKqtcUmRU41yLlI4N6ld9x';
@@ -36,7 +37,8 @@ var watchSymbols = ['$msft', '$intc', '$hpq', '$goog', '$nok', '$nvda', '$bac', 
 //This structure will keep the total number of tweets received and a map of all the symbols and how many tweets received of that symbol
 var watchList = {
     total: 0,
-    symbols: {}
+    symbols: {},
+    active: {}
 };
 
 //Set the watch symbols to zero.
@@ -58,25 +60,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 //Our only route! Render it with the current watchList
 app.get('/', function(req, res) {
-	res.render('index', { data: watchList });
+    res.render('index', { data: watchList });
 });
 
 //Start a Socket.IO listen
 var sockets = io.listen(server, {
-//Set the sockets.io configuration.
-//THIS IS NECESSARY ONLY FOR HEROKU!
-  transports: ['polling'],
-  'polling duration': 10
+    //Set the sockets.io configuration.
+    //THIS IS NECESSARY ONLY FOR HEROKU!
+    transports: ['polling'],
+    'polling duration': 10
 });
 
 //If the client just connected, give them fresh data!
-sockets.sockets.on('connection', function(socket) { 
+sockets.sockets.on('connection', function(socket) {
     socket.emit('data', watchList);
+});
+
+// Instatiate the MySQL connection
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : '', // update here
+    password : '', // update here
+    database : 'twitterCashtagHeatmap'
+});
+
+connection.connect(function(err){
+    if(err) {
+        console.log("Error connecting to MySQL database...");
+        throw(err);
+    }
+});
+
+//Clear the MySQL database
+connection.query('DELETE FROM watchList');
+connection.query('DELETE FROM history');
+connection.query('DELETE FROM history_diff');
+
+//Populate watchList
+var symbolId = 1;
+_.each(watchSymbols, function(v) {
+    connection.query("INSERT INTO watchList VALUES ("+symbolId+",'"+v+"',1)");
+    symbolId++;
 });
 
 // Instantiate the twitter connection
@@ -87,49 +116,76 @@ var t = new twitter({
     access_token_secret: access_token_secret
 });
 
-// //Tell the twitter API to filter on the watchSymbols 
+// //Tell the twitter API to filter on the watchSymbols
 t.stream('statuses/filter', { track: watchSymbols }, function(stream) {
 
-  //We have a connection. Now watch the 'data' event for incomming tweets.
-  stream.on('data', function(tweet) {
+    //We have a connection. Now watch the 'data' event for incomming tweets.
+    stream.on('data', function(tweet) {
 
-    //This variable is used to indicate whether a symbol was actually mentioned.
-    //Since twitter doesnt why the tweet was forwarded we have to search through the text
-    //and determine which symbol it was ment for. Sometimes we can't tell, in which case we don't
-    //want to increment the total counter...
-    var newCount = 0;
+        //This variable is used to indicate whether a symbol was actually mentioned.
+        //Since twitter doesnt why the tweet was forwarded we have to search through the text
+        //and determine which symbol it was ment for. Sometimes we can't tell, in which case we don't
+        //want to increment the total counter...
+        var newCount = 0;
 
-    //Make sure it was a valid tweet
-    if (tweet.text !== undefined) {
+        //Make sure it was a valid tweet
+        if (tweet.text !== undefined) {
 
-      //We're gunna do some indexOf comparisons and we want it to be case agnostic.
-      var text = tweet.text.toLowerCase();
+            //We're gunna do some indexOf comparisons and we want it to be case agnostic.
+            var text = tweet.text.toLowerCase();
 
-      //Go through every symbol and see if it was mentioned. If so, increment its counter and
-      //set the 'claimed' variable to true to indicate something was mentioned so we can increment
-      //the 'total' counter!
-      _.each(watchSymbols, function(v) {
-          if (text.indexOf(v.toLowerCase()) !== -1) {
-              watchList.symbols[v]++;
-              newCount++;
-          }
-      });
+            //Go through every symbol and see if it was mentioned. If so, increment its counter and
+            //set the 'claimed' variable to true to indicate something was mentioned so we can increment
+            //the 'total' counter!
+            _.each(watchSymbols, function(v) {
+                if (watchList.active[v]) {
+                    if (text.indexOf(v.toLowerCase()) !== -1) {
+                        watchList.symbols[v]++;
+                        newCount++;
+                    }
+                }
+            });
 
-      //If something was mentioned, increment the total counter and send the update to all the clients
-      if (newCount > 0) {
-          //Increment total
-          watchList.total += newCount;
+            //If something was mentioned, increment the total counter and send the update to all the clients
+            if (newCount > 0) {
+                //Increment total
+                watchList.total += newCount;
 
-          //Send to all the clients
-          sockets.sockets.emit('data', watchList);
-      }
-    }
-  });
+                //Send to all the clients
+                sockets.sockets.emit('data', watchList);
+            }
+        }
+    });
 });
 
 (function emit() {
-  sockets.sockets.emit('data', watchList);
-  setTimeout(emit, 10000);
+    //Update whether a symbol should be tracked or not
+    connection.query('SELECT * FROM watchList', function(err, rows) {
+        if (err) throw(err);
+        _.each(rows, function(v) {
+            watchList.active[v.cash_tag] = v.active;
+        });
+    });
+
+    for (var i = 0; i < watchSymbols.length; i++) {
+        var v = watchSymbols[i];
+        if (watchList.active[v]) {
+            var oldValue = 0;
+            connection.query("SELECT number_of_twitts FROM history WHERE symbol = '"+v+"'", function(err, rows, fields) {
+                if (typeof rows !== 'undefined' && rows.length > 0) {
+                    oldValue = rows[rows.length-1].number_of_twitts;
+                } else {
+                    oldValue = 0;
+                }
+            });
+            var diff = watchList.symbols[v] - oldValue;
+            connection.query("INSERT INTO history(when_it_happened, symbol_id, number_of_twitts) VALUES (NOW(),"+(i+1)+","+watchList.symbols[v]+")");
+            connection.query("INSERT INTO history_diff VALUES (NOW(),"+(i+1)+","+diff+")");
+        }
+    }
+
+    sockets.sockets.emit('data', watchList);
+    setTimeout(emit, 10000);
 })();
 
 // TEST
@@ -157,9 +213,21 @@ new cronJob('0 0 0 * * *', function(){
 
     //Send the update to the clients
     sockets.sockets.emit('data', watchList);
+
+    //Clear the MySQL database
+    connection.query('DELETE FROM watchList');
+    connection.query('DELETE FROM history');
+    connection.query('DELETE FROM history_diff');
+
+    //Populate watchList
+    var symbolId = 1;
+    _.each(watchSymbols, function(v) {
+        connection.query("INSERT INTO watchList VALUES ("+symbolId+",'"+v+"',1)");
+        symbolId++;
+    });
 }, null, true);
 
 //Create the server
 server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
